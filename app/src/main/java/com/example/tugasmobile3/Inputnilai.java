dfb01ec69c978bc618829e508f7a65f03187c7a3
anggirakasiwi <anggi.16411050@student.ubl.ac.id>
2019-04-13 19:40:23 +0700

    Tugas Mobile 3

---
+++ app/src/main/java/com/example/tugasmobile3/Inputnilai.java
@@ -0,0 +1,62 @@
+package com.example.tugasmobile3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class Inputnilai extends AppCompatActivity {
+
+    @BindView(R.id.edNpm)
+    EditText edNpm;
+    @BindView(R.id.edNama)
+    EditText edNama;
+    @BindView(R.id.edTugas)
+    EditText edTugas;
+    @BindView(R.id.edQuiz)
+    EditText edQuiz;
+    @BindView(R.id.edUts)
+    EditText edUts;
+    @BindView(R.id.edUas)
+    EditText edUas;
+
+    String strNpm, strNama;
+    double dblTugas, dblQuiz, dblUts, dblUas, dblHasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inputnilai);
+        ButterKnife.bind(this);
+
+        Intent getData = getIntent();
+        strNpm = getData.getStringExtra("npm");
+        strNama= getData.getStringExtra("nama");
+
+        edNpm.setText(strNpm);
+        edNama.setText(strNama);
+    }
+
+    @OnClick(R.id.btnHitung)
+    void btnHitung(){
+        dblTugas = Double.parseDouble(edTugas.getText().toString());
+        dblQuiz  = Double.parseDouble(edQuiz.getText().toString());
+        dblUts   = Double.parseDouble(edUts.getText().toString());
+        dblUas   = Double.parseDouble(edUas.getText().toString());
+
+        dblHasil = (dblTugas + dblQuiz + dblUts + dblUas) / 4;
+
+        //Toast.makeText(getApplicationContext(), String.valueOf(dblHasil),
+        //        Toast.LENGTH_LONG).show();
+        Intent a = new Intent(Inputnilai.this, Hasil.class);
+        a.putExtra("nama", strNama);
+        a.putExtra("npm", strNpm);
+        a.putExtra("nilai", String.valueOf(dblHasil));
+        startActivity(a);
+        finish();
+    }
+}
